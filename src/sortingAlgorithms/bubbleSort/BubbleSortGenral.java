6c613fe306d0e7bcae7a76233d26b68dddd748c8
karthikkb <karthikkb1012@gmail.com>
2026-03-06 00:28:36 +0530

    Bubble Sort

---
+++ src/sortingAlgorithms/bubbleSort/BubbleSortGenral.java
@@ -0,0 +1,26 @@
+package sortingAlgorithms.bubbleSort;
+
+import java.util.Random;
+
+public class BubbleSortGenral {
+    public static void main(String[] args) {
+        //int arr[] = {5, 1, 4, 2, 8};
+        int arr[] = {5, 1, 4, 2, 8, 3, 7, 6, 10, 9, 15, 12, 11, 14, 13, 20, 18, 17, 16, 19};
+        int ans[] = bubbleSort(arr);
+        for(int i=0;i<ans.length;i++){
+            System.out.print(" " + ans[i] + " ");
+        }
+    }
+    public static int[] bubbleSort(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=1;j<arr.length - i;j++){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
