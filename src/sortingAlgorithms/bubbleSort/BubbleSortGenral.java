6a324efdd8cdab44d3796061f453c8ca2ce7b068
karthikkb <karthikkb1012@gmail.com>
2026-03-06 00:34:41 +0530

    Added enhanced bubble sorting

--- src/sortingAlgorithms/bubbleSort/BubbleSortGenral.java
+++ src/sortingAlgorithms/bubbleSort/BubbleSortGenral.java
@@ -7,8 +7,13 @@ public class BubbleSortGenral {
         //int arr[] = {5, 1, 4, 2, 8};
         int arr[] = {5, 1, 4, 2, 8, 3, 7, 6, 10, 9, 15, 12, 11, 14, 13, 20, 18, 17, 16, 19};
         int ans[] = bubbleSort(arr);
+        int enhancedAns[] = bubbleSortEnhanced(arr);
         for(int i=0;i<ans.length;i++){
             System.out.print(" " + ans[i] + " ");
+        }
+        System.out.println();
+        for(int i=0;i<enhancedAns.length;i++){
+            System.out.print(" " + enhancedAns[i] + " ");
         }
     }
     public static int[] bubbleSort(int[] arr){
@@ -19,6 +24,23 @@ public class BubbleSortGenral {
                     arr[j-1] = arr[j];
                     arr[j] = temp;
                 }
+            }
+        }
+        return arr;
+    }
+    public static int[] bubbleSortEnhanced(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            boolean isSwapped = false;
+            for(int j=1;j<arr.length - i;j++){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                    isSwapped = true;
+                }
+            }
+            if(!isSwapped){
+                break;
             }
         }
         return arr;
