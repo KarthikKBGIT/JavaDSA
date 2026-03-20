85717c052e714514976dde76664e7f04aaec09c3
karthikkb <karthikkb1012@gmail.com>
2026-03-20 20:17:30 +0530

    Implemented singly linked list with insertAtFirst, insertAtLast and insertAtIndex and displayAll functions

---
+++ src/singlyLinkedList/Main.java
@@ -0,0 +1,15 @@
+package singlyLinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SingleLinkedList list = new SingleLinkedList();
+        list.insertAtFirst(1);
+        list.insertAtLast(2);
+        list.insertAtFirst(10);
+        list.insertAtIndex(4, 1);
+        list.insertAtIndex(5, 3);
+        list.displayAll();
+    }
+
+}
---
+++ src/singlyLinkedList/SingleLinkedList.java
@@ -0,0 +1,84 @@
+package singlyLinkedList;
+
+public class SingleLinkedList {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public SingleLinkedList(){
+        this.size = 0;
+    }
+
+    public void insertAtFirst(int data){
+        Node node = new Node(data);
+        node.next = head;
+        head = node;
+
+        if(tail == null){
+            tail = head;
+        }
+
+        ++size;
+    }
+
+    public void insertAtLast(int data){
+        Node node = new Node(data);
+        if(tail == null){
+            head = node;
+            tail = node;
+        }
+        else{
+            tail.next = node;
+            tail = node;
+        }
+        ++size;
+    }
+
+    public void insertAtIndex(int data, int index){
+        if(index> size || index < 0) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        if(index == 0){
+            insertAtFirst(data);
+        }
+        else if(index == size){
+            insertAtLast(data);
+        }
+        else{
+            Node node = new Node(data);
+            Node temp = head;
+            for(int i=0;i<index-1;i++){
+                temp = temp.next;
+            }
+            node.next = temp.next;
+            temp.next = node;
+            ++size;
+        }
+    }
+
+    public void displayAll(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.print("null");
+    }
+
+
+    private class Node{
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public Node(int data, Node next){
+            this.data = data;
+            this.next = next;
+        }
+    }
+}
