45f32ff6a1245938ba0f1e61585eaf25c67dda66
karthikkb <karthikkb1012@gmail.com>
2026-04-04 23:20:55 +0530

    Implemented Doubly Linked List

--- src/doublyLinkedList/DoublyLinkedList.java
+++ src/doublyLinkedList/DoublyLinkedList.java
@@ -1,4 +1,156 @@
 package doublyLinkedList;
 
 public class DoublyLinkedList {
+
+    private Node head;
+    private int size;
+
+    public DoublyLinkedList(){
+        this.size = 0;
+    }
+
+    class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+
+        public Node(int data, Node next, Node prev) {
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    public void displayAll(){
+       Node temp = head;
+       System.out.println("Accending order");
+       while(true){
+           System.out.print(temp.data + " -> ");
+           if(temp.next == null)
+               break;
+           temp = temp.next;
+       }
+       System.out.println("null");
+       System.out.println("Decending order");
+       while(temp != null){
+           System.out.print(temp.data + " -> ");
+           temp = temp.prev;
+       }
+       System.out.println("null");
+    }
+
+    public void insertAtFirst(int data){
+        Node node = new Node(data);
+        if(head == null) {
+            head = node;
+        }
+        else{
+            node.next = head;
+            head.prev = node;
+            head = node;
+        }
+        ++size;
+    }
+
+    public void insertAtLast(int data){
+        Node node = new Node(data);
+        if(head == null){
+            head = node;
+        }
+        else{
+            Node temp = head;
+            while(true){
+                if(temp.next == null){
+                    node.prev = temp;
+                    temp.next = node;
+                    break;
+                }
+                temp = temp.next;
+            }
+        }
+        ++size;
+    }
+
+    public void insertAtIndex(int data, int index){
+        if(index > size || index < 0){
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        if(index == 0){
+            insertAtFirst(data);
+        }
+        else if(index == size){
+            insertAtLast(data);
+        }
+        else{
+            Node node = new Node(data);
+            Node temp = head;
+            for(int i=0;i<index;i++){
+                temp = temp.next;
+            }
+            temp.prev.next = node;
+            node.prev = temp. prev;
+            temp.prev = node;
+            node.next = temp;
+            ++size;
+        }
+    }
+
+    public void deleteAtFirst(){
+        if(head == null){
+            throw new RuntimeException("List is empty");
+        }
+        else{
+            head = head.next;
+            if(head != null){
+                head.prev = null;
+            }
+            --size;
+        }
+    }
+
+    public void deleteAtLast(){
+        if(head == null){
+            throw new RuntimeException("List is empty");
+        }
+        else if(head.next == null){
+            head = null;
+            --size;
+        }
+        else{
+            Node temp = head;
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.prev.next = null;
+            --size;
+        }
+    }
+
+    public void deleteAtIndex(int index){
+        if(index >= size || index < 0){
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        if(index == 0){
+            deleteAtFirst();
+        }
+        else if(index == size-1){
+            deleteAtLast();
+        }
+        else{
+            Node temp = head;
+            for(int i=0;i<index;i++){
+                temp = temp.next;
+            }
+            temp.prev.next = temp.next;
+            temp.next.prev = temp.prev;
+            --size;
+        }
+    }
+
 }
--- src/doublyLinkedList/Main.java
+++ src/doublyLinkedList/Main.java
@@ -1,4 +1,29 @@
 package doublyLinkedList;
 
 public class Main {
+
+    public static void main(String[] args) {
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.insertAtFirst(10);
+        list.insertAtFirst(20);
+        list.insertAtFirst(30);
+        list.insertAtFirst(40);
+        list.insertAtLast(50);
+        list.insertAtLast(60);
+        list.insertAtIndex(25, 2);
+        list.insertAtIndex(35, 4);
+        list.insertAtIndex(45, 6);
+        System.out.println("List after insertion:");
+        list.displayAll();
+        list.deleteAtIndex(5);
+        System.out.println("List after deletion at index 5:");
+        list.displayAll();
+        list.deleteAtFirst();
+        System.out.println("List after deletion at first:");
+        list.displayAll();
+        list.deleteAtLast();
+        System.out.println("List after deletion at last:");
+        list.displayAll();
+    }
+
 }
